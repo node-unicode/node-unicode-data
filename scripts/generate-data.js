7610589480b6f4acea98d29e730b298a88ab3de9
Mathias Bynens <mathias@qiwi.be>
2013-10-01 12:55:41 +0200

    Update dependencies

--- scripts/generate-data.js
+++ scripts/generate-data.js
@@ -1,20 +1,20 @@
 var resources = require('../data/resources.js');
 var generateData = require('../index.js');
 var utils = require('../scripts/utils.js');
-var _ = require('lodash');
+var template = require('lodash.template');
 var fs = require('fs');
 var path = require('path');
 
 var templatePath = path.resolve(__dirname, '..', 'templates');
-var compileReadMe = _.template(fs.readFileSync(
+var compileReadMe = template(fs.readFileSync(
 	path.resolve(templatePath, 'README.md'),
 	'utf-8')
 );
-var compilePackage = _.template(fs.readFileSync(
+var compilePackage = template(fs.readFileSync(
 	path.resolve(templatePath, 'package.json'),
 	'utf-8')
 );
-var compileIndex = _.template(fs.readFileSync(
+var compileIndex = template(fs.readFileSync(
 	path.resolve(templatePath, 'index.js'),
 	'utf-8')
 );
