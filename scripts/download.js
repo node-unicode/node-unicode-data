04717677bcdd4c01df243144f5a3552d8e7ca1af
C. Scott Ananian <cscott@cscott.net>
2013-12-10 18:08:57 -0500

    Restructure download script to avoid exceedingly many parallel fetches.

--- scripts/download.js
+++ scripts/download.js
@@ -1,24 +1,37 @@
 var fs = require('fs');
+var guard = require('when/guard');
 var path = require('path');
 var request = require('request');
 var resources = require('../data/resources.js');
+var when = require('when');
+
+var PARALLEL_REQUEST_LIMIT = 5;
 
 var download = function(url, version, type) {
-	file = path.resolve(
+	var deferred = when.defer();
+	var file = path.resolve(
 		__dirname,
 		'..', 'data', version + '-' + type + '.txt'
 	);
-	request(url).pipe(fs.createWriteStream(file));
+	console.log(' ', url, '→', path.basename(file));
+	request(url).on('end', function() {
+		return deferred.resolve();
+	}).on('error', function(err) {
+		return deferred.reject(err);
+	}).pipe(fs.createWriteStream(file));
+	return deferred.promise;
 };
+// limit maximum parallelism to something reasonable
+download = guard(guard.n(PARALLEL_REQUEST_LIMIT), download);
 
 console.log('Downloading resources…');
 
 resources.forEach(function(resource) {
 	var version = resource.version;
 	download(resource.main, version, 'database');
-	if (resource.scripts) {
-		download(resource.scripts, version, 'scripts');
-		download(resource.blocks, version, 'blocks');
-		download(resource.properties, version, 'properties');
-	}
+	['scripts', 'blocks', 'properties'].forEach(function(p) {
+		if (resource[p]) {
+			download(resource[p], version, p);
+		}
+	});
 });
