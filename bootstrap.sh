ed81c895611415950a87af30732dc02a2120373a
Mathias Bynens <mathias@qiwi.be>
2016-05-28 23:36:13 +0200

    Update version number

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,18 +3,18 @@
 cd "$(dirname "${BASH_SOURCE}")";
 
 #rm -rf -- output/*/*!(.git);
-#npm run build;
+npm run build;
 
 cd output;
 
 for dir in $(find ./unicode-* -type d -maxdepth 0 | sort -r); do
 	cd "${dir}";
 	echo "Taking care of ${dir}…";
-	# git add -A;
-	# git commit -m 'Release v0.5.0';
-	# git tag v0.5.0;
-	# git push;
-	# git push --tags;
+	git add -A;
+	git commit -m 'Release v0.6.0';
+	git tag v0.6.0;
+	git push;
+	git push --tags;
 	npm publish;
 	cd ..;
 done;
