d48112a67c887f839d424a10895e1ce9c4870987
Mathias Bynens <mathias@qiwi.be>
2021-03-02 10:03:58 +0100

    Release v1.0.4 of each package

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,9 +11,8 @@ for dir in $(find unicode-* -maxdepth 0 -type d | sort -r); do
 	cd "${dir}";
 	echo "Taking care of ${dir}…";
 	git add -A;
-	git commit -m 'Release v1.0.3'
-	git tag v1.0.3;
+	git commit -m 'Release v1.0.4'
+	git tag v1.0.4;
 	git push;
-	git push --tags;
 	cd ..;
 done;
