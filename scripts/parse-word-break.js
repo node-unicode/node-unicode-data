181eda6a6cad0d9b517e0b1bf480420ec92b9359
Huáng Jùnliàng <jlhwung@gmail.com>
2024-11-04 18:11:30 -0500

    perf: use regenerate in parse-word-break

--- scripts/parse-word-break.js
+++ scripts/parse-word-break.js
@@ -1,43 +1,39 @@
 'use strict';
 
 const utils = require('./utils.js');
-
-const handled = new Set();
+const regenerate = require('regenerate');
 
 const parseWordBreak = function(version) {
 	const source = utils.readDataFile(version, 'word-break');
 	if (!source) {
 		return;
 	}
-	const map = {};
+	const map = {
+		// All code points not explicitly listed have the value `Other` (`XX`).
+		'Other': regenerate().addRange(0, 0x10FFFF)
+	};
 	const lines = source.split('\n');
-	lines.forEach(function(line) {
+	for (const line of lines) {
 		if (!line || /^#/.test(line)) {
-			return;
+			continue;
 		}
 		const data = line.trim().split(';');
 		const charRange = data[0].replace('..', '-').trim();
 		const rangeParts = charRange.split('-');
 		const value = data[1].split('#')[0].trim();
 		const canonicalName = value;
+		map[canonicalName] ??= regenerate();
 		if (rangeParts.length == 2) {
-			utils.range(
+			const [from, to] = [
 				parseInt(rangeParts[0], 16),
-				parseInt(rangeParts[1], 16)
-			).forEach((codePoint) => {
-				utils.append(map, canonicalName, codePoint);
-				handled.add(codePoint);
-			});
+				parseInt(rangeParts[1], 16),
+			];
+			map['Other'].removeRange(from, to);
+			map[canonicalName].addRange(from, to);
 		} else {
 			const codePoint = parseInt(charRange, 16);
-			utils.append(map, canonicalName, codePoint);
-			handled.add(codePoint);
-		}
-	});
-	// All code points not explicitly listed have the value `Other` (`XX`).
-	for (let codePoint = 0x000000; codePoint <= 0x10FFFF; codePoint++) {
-		if (!handled.has(codePoint)) {
-			utils.append(map, 'Other', codePoint);
+			map['Other'].remove(codePoint);
+			map[canonicalName].add(codePoint);
 		}
 	}
 	return map;
