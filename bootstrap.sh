7a4607bc2fdbccb94134d5f85d2b158f2087dea7
Mathias Bynens <mathias@qiwi.be>
2024-11-08 15:16:12 +0100

    Tag the v1.6.4 releases

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,8 +11,8 @@ for dir in $(find unicode-* -maxdepth 0 -type d | sort -r); do
 	cd "${dir}";
 	echo "Taking care of ${dir}…";
 	git add -A;
-	git commit -m 'Release v1.6.3';
-	git tag v1.6.3;
+	git commit -m 'Release v1.6.4';
+	git tag v1.6.4;
 	git push;
 	git push --tags;
 	cd ..;
