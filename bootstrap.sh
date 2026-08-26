104c1a523104df28d93b8a996fd98f20a374b5e0
Mathias Bynens <mathias@qiwi.be>
2026-08-26 16:21:19 +0200

    Create the v2.0.2 releases

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,8 +11,8 @@ for dir in $(find unicode-* -maxdepth 0 -type d | sort -r); do
 	cd "${dir}";
 	echo "Taking care of ${dir}…";
 	git add -A;
-	git commit -m 'Release v2.0.1';
-	git tag v2.0.1;
+	git commit -m 'Release v2.0.2';
+	git tag v2.0.2;
 	git push;
 	git push --tags;
 	cd ..;
