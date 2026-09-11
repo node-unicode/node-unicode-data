f4fbac2fde06eb2efb6542cce4f4835f9160f9c3
Mathias Bynens <mathias@qiwi.be>
2026-09-11 14:32:13 +0200

    Tag the v2.0.6 releases

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,9 +11,9 @@ for dir in $(find unicode-* -maxdepth 0 -type d | sort -r); do
 	cd "${dir}";
 	echo "Taking care of ${dir}…";
 	git add -A;
-	git commit -m 'Release v2.0.5';
+	git commit -m 'Release v2.0.6';
 	# When tagging a release, update `templates/package.json` accordingly.
-	git tag v2.0.5;
+	git tag v2.0.6;
 	git push;
 	git push --tags;
 	cd ..;
